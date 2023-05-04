008b831fc3834044b6522673b3768dc3f175136a
drewbear82 <andrewkentjohnston@gmail.com>
2023-05-03 22:27:16 -0600

    created Caesar Cypher programme

---
+++ main.py
@@ -0,0 +1,24 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--file")
+parser.add_argument("--offset")
+args = parser.parse_args()
+
+filename = args.file
+offset = int(args.offset)
+opened_file = open(filename)
+encoded_text = opened_file.read()  # read the file into a string
+opened_file.close()  # always close the files you've opened
+
+
+def decode_Caesar_cipher(s, n):
+    alpha = " ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz',.?!"
+    s = s.strip()
+    text = ""
+    for c in s:
+        text += alpha[(alpha.index(c) + n) % len(alpha)]
+    print('Decoded text: "' + text + '"')
+
+
+decode_Caesar_cipher(encoded_text, offset)
